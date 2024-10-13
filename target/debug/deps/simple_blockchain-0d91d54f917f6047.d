722a9199e3e7b19e0e1bd643c91450e2d4c95c35
tugi-os <tugrulozaltan@gmail.com>
2024-10-13 13:14:59 +0300

    Added Cargo.toml and main.rs

---
+++ target/debug/deps/simple_blockchain-0d91d54f917f6047.d
@@ -0,0 +1,5 @@
+C:\Users\Tuğrul\simple_blockchain\target\debug\deps\libsimple_blockchain-0d91d54f917f6047.rmeta: src/main.rs
+
+C:\Users\Tuğrul\simple_blockchain\target\debug\deps\simple_blockchain-0d91d54f917f6047.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/simple_blockchain-b4f0e1985150a5cc.d
@@ -0,0 +1,5 @@
+C:\Users\Tuğrul\simple_blockchain\target\debug\deps\libsimple_blockchain-b4f0e1985150a5cc.rmeta: src/main.rs
+
+C:\Users\Tuğrul\simple_blockchain\target\debug\deps\simple_blockchain-b4f0e1985150a5cc.d: src/main.rs
+
+src/main.rs:
